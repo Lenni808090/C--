f93fd45fc23f92ebc4f768ef7d1f3cfaaf826c85
Lenni808090 <haselenni@gmail.com>
2026-06-26 20:10:09 +0200

    jit memory

---
+++ native-jit/Headers/JitMemory.h
@@ -0,0 +1,10 @@
+//
+// Created by leona on 26.06.2026.
+//
+
+#ifndef NATIVE_JIT_JITMEMORY_H
+#define NATIVE_JIT_JITMEMORY_H
+
+
+
+#endif //NATIVE_JIT_JITMEMORY_H
---
+++ native-jit/JitMemory.c
@@ -0,0 +1,5 @@
+//
+// Created by leona on 26.06.2026.
+//
+
+#include "JitMemory.h"
